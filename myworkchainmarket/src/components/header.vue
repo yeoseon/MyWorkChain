1ec631507d1379e72cd286e725d036d079cf8966
keunyop <kylee1112@hotmail.com>
2019-08-25 11:31:26 +0900

    maket api

--- myworkchainmarket/src/components/header.vue
+++ myworkchainmarket/src/components/header.vue
@@ -1,12 +1,9 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
     <div class="container-fluid px-3">
-      <router-link class="navbar-brand text-big font-weight-700 line-height-1 py-3" :to="{ name: 'home' }">MyWorkChain 마켓</router-link>
+      <router-link class="navbar-brand text-big font-weight-700 line-height-1 py-3" :to="{ name: 'home' }">MWC Market</router-link>
 
       <div class="navbar-collapse" id="landing-navbar-collapse">
-        <div class="navbar-nav align-items-lg-center ml-lg-4">
-          <router-link class="nav-link font-weight-700" :class="{selected: $route.path.indexOf('product') !== -1}" :to="{ name: 'product' }">상품</router-link>
-        </div>
         <div class="navbar-nav align-items-lg-center ml-auto">
           <div class="nav-item">
             <router-link class="btn btn-outline-white btn-round" :to="{ name: 'my' }">
