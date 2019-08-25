817f2cee951d14c8061f534bf88d623bfa02dbba
seongbeom.jeong <jsacc0619@gmail.com>
2019-08-26 01:08:02 +0900

    업무 시작/종료 에 따른 데이터베이스 및 View 처리 완료

--- myworkchainapp/src/main/java/bankware/finlab/myworkchain/app/controller/MyWorkChainAppController.java
+++ myworkchainapp/src/main/java/bankware/finlab/myworkchain/app/controller/MyWorkChainAppController.java
@@ -1,5 +1,7 @@
 package bankware.finlab.myworkchain.app.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,9 +27,17 @@ public class MyWorkChainAppController {
 	@Autowired
 	private WorkHistoryService workHistoryService;
 	
+	// 하드 코딩, 추후 변경 필요 TODO
+	private static final String EMPL_ADDRESS = "0xB31794ef274FFb1e6e4a55bAE4f9F18DeBA3C112";
+	
 	@GetMapping("/")
 	public String viewHome(Model model) {
 		if(logger.isDebugEnabled()) logger.debug("viewHome {}", model);
+		
+		// Model Attributes
+		List<WorkHistoryEntity> workHistoryEntityList = workHistoryService.getWorkHistoryList(EMPL_ADDRESS);
+		logger.info("workHistoryEntityList : {}", workHistoryEntityList);
+		model.addAttribute("workHistoryEntityList", workHistoryEntityList);
 		
 		// template name
 		return "home";
@@ -41,13 +51,22 @@ public class MyWorkChainAppController {
 		return "login";
 	}
 	
-	@GetMapping("/work/history/{userAddress:.+}")
-	public @ResponseBody WorkHistoryEntity getWorkHistory(@PathVariable("userAddress") String userAddress) {
+	@GetMapping("/work/history/{userAddress:.+}/{workStartYmd}")
+	public @ResponseBody WorkHistoryEntity getWorkHistory(@PathVariable("userAddress") String userAddress, @PathVariable("workStartYmd") String workStartYmd) {
 		logger.info("userAddress : {}", userAddress);
-		WorkHistoryEntity workHistoryEntity = workHistoryService.getWorkHistory(userAddress);
+		WorkHistoryEntity workHistoryEntity = workHistoryService.getWorkHistoryWithWorkStartYmd(userAddress, workStartYmd);
 		logger.info("workHistoryEntity : {}", workHistoryEntity);
 		
 		return workHistoryEntity;
+	}
+	
+	@GetMapping("/work/history/{userAddress:.+}")
+	public @ResponseBody List<WorkHistoryEntity> getWorkHistoryList(@PathVariable("userAddress") String userAddress) {
+		logger.info("userAddress : {}", userAddress);
+		List<WorkHistoryEntity> workHistoryEntityList = workHistoryService.getWorkHistoryList(userAddress);
+		logger.info("workHistoryEntityList : {}", workHistoryEntityList);
+		
+		return workHistoryEntityList;
 	}
 	
 	@Transactional
--- myworkchainapp/src/main/java/bankware/finlab/myworkchain/app/service/WorkHistoryService.java
+++ myworkchainapp/src/main/java/bankware/finlab/myworkchain/app/service/WorkHistoryService.java
@@ -2,6 +2,7 @@ package bankware.finlab.myworkchain.app.service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -37,13 +38,23 @@ public class WorkHistoryService {
 	/**
 	 * Work History 가져오기
 	 * @param userAddress
+	 * @param workStartYmd
 	 * @return
 	 */
-	public WorkHistoryEntity getWorkHistory(String userAddress) {
-		LocalDateTime localDateTime = LocalDateTime.now();
-		String localDate = localDateTime.toLocalDate().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+	public WorkHistoryEntity getWorkHistoryWithWorkStartYmd(String userAddress, String workStartYmd) {
+//		LocalDateTime localDateTime = LocalDateTime.now();
+//		String localDate = localDateTime.toLocalDate().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
 		
-		return workHistoryRepository.findWorkHistoryByUserAddressAndWorkStartYmd(userAddress, localDate);
+		return workHistoryRepository.findWorkHistoryByUserAddressAndWorkStartYmd(userAddress, workStartYmd);
+	}
+	
+	/**
+	 * Work History 목록 가져오기
+	 * @param userAddress
+	 * @return
+	 */
+	public List<WorkHistoryEntity> getWorkHistoryList(String userAddress) {
+		return workHistoryRepository.findWorkHistoryByUserAddress(userAddress);
 	}
 	
 	/**
@@ -56,7 +67,7 @@ public class WorkHistoryService {
 		String localDate = localDateTime.toLocalDate().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
 		String localTime = localDateTime.toLocalTime().format(DateTimeFormatter.ofPattern("HHmmss"));
 		
-		WorkHistoryEntity oldWorkHistoryEntity = getWorkHistory(workHistoryDto.getUserAddress());
+		WorkHistoryEntity oldWorkHistoryEntity = getWorkHistoryWithWorkStartYmd(workHistoryDto.getUserAddress(), localDate);
 		WorkHistoryEntity workHistoryEntity = WorkHistoryEntity.builder()
 																.id(oldWorkHistoryEntity.getId())
 																.userAddress(oldWorkHistoryEntity.getUserAddress())
