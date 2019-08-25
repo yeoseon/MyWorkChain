417f5f802741a08d30f4c30db3765b4c9b901057
seongbeom.jeong <jsacc0619@gmail.com>
2019-08-26 01:03:54 +0900

    메소드 추가

--- myworkchain-common/src/main/java/bankware/finlab/myworkchain/common/repository/WorkHistoryRepository.java
+++ myworkchain-common/src/main/java/bankware/finlab/myworkchain/common/repository/WorkHistoryRepository.java
@@ -1,5 +1,7 @@
 package bankware.finlab.myworkchain.common.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import bankware.finlab.myworkchain.common.entity.WorkHistoryEntity;
@@ -7,5 +9,6 @@ import bankware.finlab.myworkchain.common.entity.WorkHistoryEntity;
 public interface WorkHistoryRepository extends CrudRepository<WorkHistoryEntity, Long> {
 	
 	WorkHistoryEntity findWorkHistoryByUserAddressAndWorkStartYmd(String userAddress, String workStartYmd);
-	
+
+	List<WorkHistoryEntity> findWorkHistoryByUserAddress(String userAddress);
 }
