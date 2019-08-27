6efa93cc6cd4dfb61704249555806bab05b42904
seongbeom.jeong <seongbeom.jeong@bankwareglobal.com>
2019-08-27 17:11:49 +0900

    LocalDate => LocalDateTime 으로 변경

--- myworkchain-common/src/main/java/bankware/finlab/myworkchain/common/repository/WorkHistoryRepository.java
+++ myworkchain-common/src/main/java/bankware/finlab/myworkchain/common/repository/WorkHistoryRepository.java
@@ -1,6 +1,6 @@
 package bankware.finlab.myworkchain.common.repository;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
@@ -9,7 +9,7 @@ import bankware.finlab.myworkchain.common.entity.WorkHistoryEntity;
 
 public interface WorkHistoryRepository extends CrudRepository<WorkHistoryEntity, Long> {
 	
-	List<WorkHistoryEntity> findWorkHistoryByUserIdAndTimeBetween(String userId, LocalDate now, LocalDate after);
+	List<WorkHistoryEntity> findWorkHistoryByUserIdAndTimeBetween(String userId, LocalDateTime now, LocalDateTime after);
 
 	List<WorkHistoryEntity> findWorkHistoryByUserId(String userId);
 }
