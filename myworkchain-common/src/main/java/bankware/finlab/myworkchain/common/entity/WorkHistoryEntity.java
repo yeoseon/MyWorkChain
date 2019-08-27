85a7449daefaf196eb082533f9b116bdcd9add1e
seongbeom.jeong <seongbeom.jeong@bankwareglobal.com>
2019-08-27 13:48:01 +0900

    Timestamp => LocalDateTime 으로 변경

--- myworkchain-common/src/main/java/bankware/finlab/myworkchain/common/entity/WorkHistoryEntity.java
+++ myworkchain-common/src/main/java/bankware/finlab/myworkchain/common/entity/WorkHistoryEntity.java
@@ -1,12 +1,15 @@
 package bankware.finlab.myworkchain.common.entity;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -23,7 +26,8 @@ public class WorkHistoryEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String userId;
-	private Timestamp time;
+	@CreationTimestamp
+	private LocalDateTime time;
 	private String workCode;
 	private int latitude;
 	private int longitude;
@@ -31,7 +35,7 @@ public class WorkHistoryEntity {
 	private int reward;
 	
 	@Builder
-	public WorkHistoryEntity(int id, String userId, Timestamp time, String workCode, int latitude, int longitude, String workPlaceCode, int reward) {
+	public WorkHistoryEntity(int id, String userId, LocalDateTime time, String workCode, int latitude, int longitude, String workPlaceCode, int reward) {
 		this.id = id;
 		this.userId = userId;
 		this.time = time;
