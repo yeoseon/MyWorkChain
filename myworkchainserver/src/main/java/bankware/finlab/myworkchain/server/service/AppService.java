5d782d40ed12b6c10ec84fb355ff8559c5672d15
Yun Yeoseon <devyyskr@gmail.com>
2019-08-28 08:57:23 +0900

    근무 기록 서비스 주석 처리

--- myworkchainserver/src/main/java/bankware/finlab/myworkchain/server/service/AppService.java
+++ myworkchainserver/src/main/java/bankware/finlab/myworkchain/server/service/AppService.java
@@ -11,11 +11,12 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 
 import bankware.finlab.myworkchain.app.dto.WorkHistoryDto;
 import bankware.finlab.myworkchain.app.service.WorkHistoryService;
+import bankware.finlab.myworkchain.common.constant.DataSourceConstant;
 import bankware.finlab.myworkchain.common.constant.WorkHistoryConstant;
 import bankware.finlab.myworkchain.common.entity.WorkHistoryEntity;
 import bankware.finlab.myworkchain.common.entity.WorkPlaceEntity;
 import bankware.finlab.myworkchain.common.repository.EmployeeRepository;
-import bankware.finlab.myworkchain.server.dto.NewWorkHistoryToChainRequest;
+import bankware.finlab.myworkchain.server.dto.SendRewardRequest;
 
 @Service
 public class AppService {
@@ -55,24 +56,24 @@ public class AppService {
 		Date time = new Date(); 
 		input.setTime(time);
 		
-		Boolean result = false;
-		
-		//1. 근무 기록 (to DB) TODO
-		Boolean newWorkToDBResponse = true;
-		WorkHistoryEntity workHistoryDbItem = workHistoryService.newWorkHistoryToDB(input); 
-		
-		//2. 근무 기록 (to Chain)
-		Boolean newWorkToChainResponse = workService.newWorkHistoryToChain(input);
-		
-		//3. 토큰 대상 여부 검사 TODO
-//		Boolean isSendReward = _isSendReward(newWorkToChainRequest);
+//		Boolean result = false;
+//		
+//		//1. 근무 기록 (to DB) TODO
+//		WorkHistoryEntity workHistoryDbItem = workHistoryService.newWorkHistoryToDB(input); 
+//		Boolean newWorkToDBResponse = true; // TODO: DB등록 잘 되었는지 처리
+//		
+//		//2. 근무 기록 (to Chain)
+//		Boolean newWorkToChainResponse = workService.newWorkHistoryToChain(input);
+//		
+//		//3. 토큰 대상 여부 검사 TODO
+//		Boolean isSendReward = _isSendReward(input);
 //		
 //		//4. 토큰 대상일 경우 토큰 지급
 //		Boolean sendRewardResponse = false;
 //		if(isSendReward) {
 //			SendRewardRequest sendRewardRequest = new SendRewardRequest();
-//			sendRewardRequest.setGiverAddress(employeeRepository.findEmplAddressById(input.getId())); //직원이 속한 기업 Address 호출하여 Setting
-//			sendRewardRequest.setReceiverAddress(employeeRepository.findEmployeeById(input.getId()).getEmplAddress()); //직원 id를 통해 Address 호출하여 Setting
+//			sendRewardRequest.setGiverAddress(employeeRepository.findEmplAddressByUserId(input.getUserId())); //직원이 속한 기업 Address 호출하여 Setting
+//			sendRewardRequest.setReceiverAddress(employeeRepository.findEmployeeByUserId(input.getUserId()).getEmplAddress()); //직원 id를 통해 Address 호출하여 Setting
 //			sendRewardRequest.setValueAmount(DataSourceConstant.VALUE_AMOUNT); 
 //			sendRewardResponse = rewardService.sendReward(sendRewardRequest);
 //		}
@@ -83,10 +84,9 @@ public class AppService {
 //		if(newWorkToChainResponse == true && sendRewardResponse == true && newWorkToDBResponse == true) {
 //			result = true;
 //		}
-//		
-//		return result;
 		
 		return true;
+//		return result;
 	}
 	
 	/*
@@ -94,7 +94,7 @@ public class AppService {
 	 * 퇴근 시, 출근 시간과 비교하여 8시간 이상 근무를 했을 경우 토큰을 지급한다.
 	 * TODO: result가 false시, 에러 메시지도 처리해야할 듯.
 	 */
-	private Boolean _isSendReward(NewWorkHistoryToChainRequest request) throws ParseException {
+	private Boolean _isSendReward(WorkHistoryDto request) throws ParseException {
 		
 		Boolean result = false;
 
@@ -103,19 +103,19 @@ public class AppService {
 		WorkPlaceEntity userWorkPlace = companyService.getWorkPlaceByCode(userWorkPlaceCode);
 		
 		// 입력받은 위/경도와 비교 TODO : 기준 정확히 정해서 구현해둘 것
-//		if(userWorkPlace.getLatitude() == request.getLatitude() && userWorkPlace.getLongitude() == request.getLongtitude()) {
-//			result = true;
-//		}
-//		else {
-//			return false; // 현재 위치는 근무지가 아닙니다.
-//		}
+		if(userWorkPlace.getLatitude() == request.getLatitude() && userWorkPlace.getLongitude() == request.getLongitude()) {
+			result = true;
+		}
+		else {
+			return false; // 현재 위치는 근무지가 아닙니다.
+		}
 		
 		if(WorkHistoryConstant.WORK_CODE_START.equals(request.getWorkCode())) {
 			//WorkCode가 '출근'인 경우
 			//오전 09:30분 이전 출근 등록시 true
 			
 			SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
-			Date date = format.parse(request.getDate().toString());
+			Date date = format.parse(request.getTime().toString());
 			Date referenceDate = format.parse("09:30:00"); //오전 9시 30분
 			
 			int compare = referenceDate.compareTo(date); //date가 더 작아야 함. 즉 compare가 > 0
